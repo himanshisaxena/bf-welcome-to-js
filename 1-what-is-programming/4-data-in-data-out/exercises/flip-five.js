d6cf181b34254bcc663bb2dca9ce4ef30c0fbc04
himanshisaxena <saxena.himanshi11@gmail.com>
2023-05-26 11:17:39 +0200

    understand what is programming in javascript

--- 1-what-is-programming/4-data-in-data-out/exercises/flip-five.js
+++ 1-what-is-programming/4-data-in-data-out/exercises/flip-five.js
@@ -1,15 +1,19 @@
 'use strict';
 
 /* Flip Five
-
+This program reverses the input only if it is 10 characters long.
   Data In:
-
+  Any text
 
   Data Out:
-
+  the reversed input if it was 10 characters long
+  the unchanged input if it was not 10 characters long
 
   Test Cases:
-
+  '' -> ''
+  'abc' -> 'abc'
+  'JavaScript' -> 'tpircSavaJ'
+  'HimanshiSaxena' -> 'HimanshiSaxena'
 
 */
 
@@ -17,12 +21,12 @@
 
 let input = null;
 while (true) {
-  input = prompt('enter something with 5 characters and it will be reversed.');
+  input = prompt('enter something with 10 characters and it will be reversed.');
   console.log(input);
 
-  /* --- check that the user input is 5 characters long --- */
+  /* --- check that the user input is 10 characters long --- */
 
-  if (input !== null && input.length === 5) {
+  if (input !== null && input.length === 10) {
     break;
   } else {
     alert('nope, try again.');
